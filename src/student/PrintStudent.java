1a2169136d31c6ac0086c9531b3afb829c398edd
khatterd <deveshkhatter@gmail.com>
2022-05-25 14:51:28 -0400

    version 1

--- src/student/PrintStudent.java
+++ src/student/PrintStudent.java
@@ -2,11 +2,10 @@ package student;
 
 /**
  * model class - raw data for student template
- * 
+ *  25/05/2022
  */
 public class PrintStudent 
 {
     String name;
     public int age = 40;
-    
 }
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,4 +1,5 @@
 package student;
+
 public class Student 
 {
     public static void main(String[] args) 
